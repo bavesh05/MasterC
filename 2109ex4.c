8e22d201fe3f55b5b543447cf062b0464c40ca27
bavesh05 <baveshkrishna123@gmail.com>
2023-09-24 14:52:54 +0530

    odd numbers

---
+++ 2109ex4.c
@@ -0,0 +1,20 @@
+/*authour:bavesh
+  date:21-09-23
+  purpose:to print all odd numbers between 1 to 100*/
+#include <stdio.h>
+int main()
+{
+int a;
+a=1;
+
+while(a<=100){
+
+if(a%2!=0){
+
+ printf("%d",a);
+}
+
+ a++;
+}
+
+}
