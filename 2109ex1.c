edb8a61d5f298e9e1ccbafa9eee0b2560388f86b
bavesh05 <baveshkrishna123@gmail.com>
2023-09-24 12:23:15 +0530

    jjjj

---
+++ 2109ex1.c
@@ -0,0 +1,18 @@
+/*authour: bavesh
+  date:21-09-23
+  purpose:to print all natural numbers from 1 to n*/
+#include <stdio.h>
+int main()
+{
+ int a,n;
+ a=1;
+ printf("enter n value:");
+ scanf("%d",&n);
+
+ while(a<=n){
+printf("%d",a);
+a++; 
+}
+
+
+}
