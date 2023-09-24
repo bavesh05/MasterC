fb3db1dee7da6a2679cc4a921b5d9ce1ab4d022f
bavesh05 <baveshkrishna123@gmail.com>
2023-09-24 14:42:06 +0530

    nn

---
+++ 2109ex2.c
@@ -0,0 +1,17 @@
+/*authour:bavesh
+  date:21-09-23
+  purpose:to print numbers from n to 1*/
+#include <stdio.h>
+int main()
+{
+ int a,n;
+ a=1;
+ printf("enter n value");
+ scanf("%d",&n);
+
+ while(n>=a){
+printf("%d",n);
+n--;
+}
+
+}
