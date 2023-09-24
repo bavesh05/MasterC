0223119616ff8cb130a034d134cad93e0723bc72
bavesh05 <baveshkrishna123@gmail.com>
2023-09-24 14:43:28 +0530

    nnn

---
+++ 2109ex3.c
@@ -0,0 +1,17 @@
+/*authour:bavesh
+  date:21-09-23
+  purpose:to print all even numbers between 1 to 100*/
+#include <stdio.h>
+int main()
+{
+ int a;
+ a=1;
+ while (a<=100){
+ 
+ if(a%2==0){
+ printf("%d",a);
+}
+ a++;
+}
+
+}
